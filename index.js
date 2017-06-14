a2e7bb0602de308b81312883a38f7a84d9759fc2
lidong <498437323@qq.com>
2017-06-14 11:12:27 +0800

    ng-route

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
  * Created by Administrator on 2017/6/8.
  */
 import angular from 'angular';
+import angularRoute from 'angular-route';
 import style from './src/less/index.less';
-const app = angular.module('app', []);
-app.controller('ctr1', ['$scope', '$timeout', function ($scope, $timeout) {
+const app = angular.module('app', ['ngRoute']);
+app.controller('ctr1', ['$scope', '$timeout', '$routeParams',function ($scope, $timeout,$routeParams) {
     $scope.user = {
         name: 'lidong',
         age: '26',
@@ -13,7 +14,8 @@ app.controller('ctr1', ['$scope', '$timeout', function ($scope, $timeout) {
     $scope.htmlt = "<span class='red'>11</span>";
     $timeout(function () {
         $scope.user.name = '李栋'
-    }, 3000)
+    }, 3000);
+    console.log($routeParams)
 }]);
 app.directive('hello',function(){
     return {
@@ -22,3 +24,10 @@ app.directive('hello',function(){
         transclude:true
     }
 });
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/',{template:'这是首页页面'})
+        .when('/computers/:id?',{template:'这是电脑分类页面'})
+        .when('/printers',{template:'这是打印机页面'})
+        .otherwise({redirectTo:'/'});
+}]);
